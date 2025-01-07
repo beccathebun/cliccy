a47e7842edd77610533d693c20779215dba062ac
beccathebun <rebeccap2688@gmail.com>
2025-01-07 17:40:18 +0100

    add option to disable time on logger

--- include/log.h
+++ include/log.h
@@ -6,46 +6,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdarg.h>
-#define T_RESET "\x1b[0m"
-#define FG_BLK        "\x1b[30m"
-#define FG_RED        "\x1b[31m"
-#define FG_GRN        "\x1b[32m"
-#define FG_YLW        "\x1b[33m"
-#define FG_BLU        "\x1b[34m"
-#define FG_MAG        "\x1b[35m"
-#define FG_CYN        "\x1b[36m"
-#define FG_WHT        "\x1b[37m"
-#define FG_DEF        "\x1b[39m"
-#define FG_BBLK       "\x1b[90m"
-#define FG_BRED       "\x1b[91m"
-#define FG_BGRN       "\x1b[92m"
-#define FG_BYLW       "\x1b[93m"
-#define FG_BBLU       "\x1b[94m"
-#define FG_BMAG       "\x1b[95m"
-#define FG_BCYN       "\x1b[96m"
-#define FG_BWHT       "\x1b[97m"
-#define FG_256(id)    "\x1b[38;5;" #id "m"
-#define FG_RGB(r,g,b) "\x1b[38;2;" #r ";" #g ";" #b "m"
-
-#define BG_BLK        "\x1b[40m"
-#define BG_RED        "\x1b[41m"
-#define BG_GRN        "\x1b[42m"
-#define BG_YLW        "\x1b[43m"
-#define BG_BLU        "\x1b[44m"
-#define BG_MAG        "\x1b[45m"
-#define BG_CYN        "\x1b[46m"
-#define BG_WHT        "\x1b[47m"
-#define BG_DEF        "\x1b[49m"
-#define BG_BBLK       "\x1b[100m"
-#define BG_BRED       "\x1b[101m"
-#define BG_BGRN       "\x1b[102m"
-#define BG_BYLW       "\x1b[103m"
-#define BG_BBLU       "\x1b[104m"
-#define BG_BMAG       "\x1b[105m"
-#define BG_BCYN       "\x1b[106m"
-#define BG_BWHT       "\x1b[107m"
-#define BG_256(id)    "\x1b[48;5;" #id "m"
-#define BG_RGB(r,g,b) "\x1b[48;2;" #r ";" #g ";" #b "m"
+#include "colour.h"
 
 #ifndef FG_INFO
 #define FG_INFO FG_256(105)
@@ -93,6 +54,7 @@ extern void logs(int msgType, const char *text, ...);
 
 // set minimum log level for logger
 extern void set_log_level(int log_level);
+static bool logger_print_time = true;
 #endif // _LOG_H_
 #if defined(LOGGER_IMPL)
 static LoggerLogLevel __logger_log_lvl = Log_Info;
@@ -100,12 +62,15 @@ static LoggerLogLevel __logger_log_lvl = Log_Info;
 extern void vlogs(int msgType, const char *text, va_list args)
 {
     if(msgType < __logger_log_lvl) return;
-    char timeStr[64] = { 0 };
-    time_t now = time(NULL);
-    struct tm *tm_info = localtime(&now);
-
-    strftime(timeStr, sizeof(timeStr), "%H:%M:%S", tm_info);
-    printf(FG_256(255)"[%s] ", timeStr);
+    
+    if(logger_print_time) {
+      char timeStr[64] = { 0 };
+      time_t now = time(NULL);
+      struct tm *tm_info = localtime(&now);
+      strftime(timeStr, sizeof(timeStr), "%H:%M:%S", tm_info);
+      printf(FG_256(255)"[%s] ", timeStr);
+    }
+    
 
     switch (msgType)
     {
