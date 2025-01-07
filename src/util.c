1bc5ce65e185e7f4c88c002ada9835cae9e062ed
beccathebun <rebeccap2688@gmail.com>
2025-01-07 11:34:38 +0100

    compiler errors fix

--- src/util.c
+++ src/util.c
@@ -21,7 +21,7 @@ bool __startsWith_(const char *pre, const char *str, bool ci)
 {
     size_t lenpre = strlen(pre),
            lenstr = strlen(str);
-    char *cmp     = _CALLOC(lenpre + 1, sizeof(char));
+    char *cmp     = (char*)(_CALLOC(lenpre + 1, sizeof(char)));
     strncpy(cmp, str, lenpre);
 
     if(ci) for(char *p = cmp; *p; ++p) *p = tolower(*p);
@@ -38,7 +38,7 @@ bool __startsWith_(const char *pre, const char *str, bool ci)
 #define istartsWith(prefix, string) __startsWith_(prefix,string,true)
 
 char *__concat_(char *s1, char *s2, bool space, uint8_t nexc) {
-  char *result = _CALLOC(strlen(s1) + strlen(s2) + (space ? 2 : 1) + nexc, sizeof(char));
+  char *result = (char*)(_CALLOC(strlen(s1) + strlen(s2) + (space ? 2 : 1) + nexc, sizeof(char)));
   char *nb     = stpcpy(result, s1);
   if(nexc) for(;nexc > 0; --nexc,++nb) *nb='!'; 
   if(space) *nb = ' ';
